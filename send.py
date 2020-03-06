e41c677428054eabedfc383575aa0369e706adab
ErikBoesen <me@erikboesen.com>
2020-03-06 14:21:13 -0500

    Read token from file if necessary

--- send.py
+++ send.py
@@ -8,12 +8,16 @@ MESSAGES = [
     "good morning",
     "good morning brothers",
     "Hello brothers good morning",
-    "goodmorning 😁",
+    "goodmorning",
+    "Goodmorning",
     "Good morning to all the brothers",
 ]
 
+token = os.environ["GROUPME_ACCESS_TOKEN"]
+if not token:
+    with open(os.environ["HOME"] + "/groupme_token.txt", "r") as f:
+        token = f.read().strip()
 
-client = Client.from_token(os.environ["GROUPME_ACCESS_TOKEN"])
+client = Client.from_token(token)
 group = client.groups.get(id=GROUP_ID)
-#group.leave()
 group.post(text=random.choice(MESSAGES))
